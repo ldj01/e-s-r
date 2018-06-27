f1df40253d1e77bcffefb238f905024f7cb2887f
Lowell Johnson <lowell.johnson.ctr@usgs.gov>
2018-06-27 10:04:47 -0500

    LDEV-734 (HDFEOS->GCTP2), 747 (cmake), 748 (LPGS install), 781 (rm IAS libs)
    LDEV-734 Remove HDFEOS dependencies.
    LDEV-747 Convert to CMake build architecture.
             Remove XML2 library env vars in CMake, switching to primary use of
             system-installed libxml2-dev package.
             Remove unused ZLIBINC variable.
             Add INSTALL file with CMake instructions.
    LDEV-748 Use install PREFIX for ESPA library links.  If the EPSAINC or
             ESPALIB variables aren't defined as CMake input or environment
             variables, use the CMAKE_INSTALL_PREFIX path.
    LDEV-781 Replace ESPA library use with IPS version.

--- lasrc/c_version/src/quick_select.c
+++ lasrc/c_version/src/quick_select.c
@@ -15,7 +15,7 @@ This Quickselect routine is based on the algorithm described in
   Cambridge University Press, 1992, Section 8.5, ISBN 0-521-43108-5
   This code by Nicolas Devillard - 1998. Public domain.
 *****************************************************************************/
-#include <quick_select.h>
+#include "quick_select.h"
 
 /******************************************************************************
 MODULE:  swap
